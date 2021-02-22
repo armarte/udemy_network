5513292b7f58d7d155dfd22f33c1d8e729cd9a13
Armin Marten <armin.marten@intenta.de>
2021-02-22 15:52:56 +0100

    Added a test c file with a template main function.

---
+++ test.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  test c file
+ *
+ *        Version:  1.0
+ *        Created:  22.02.2021 15:28:43
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Armin Marten (armin.marten@intenta.de), 
+ *   Organization:  Intenta GmbH
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **ragv)
+{
+    return 0;
+}
